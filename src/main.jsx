49dbda019380ab0a8cfc3cfa7798f3a8ee76d05c
orangescrypto-jpg <orangescrypto@gmail.com>
2026-05-03 14:52:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { DashboardProvider } from './context/DashboardContext'
+import ErrorBoundary from './components/common/ErrorBoundary'
 import AppRoutes from './app/router'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <DashboardProvider>
-          <AppRoutes />
-        </DashboardProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <AuthProvider>
+          <DashboardProvider>
+            <AppRoutes />
+          </DashboardProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
