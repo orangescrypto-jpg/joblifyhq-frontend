3a36dc93c85b1a9e298006b558c95bd781a873d6
orangescrypto-jpg <orangescrypto@gmail.com>
2026-05-03 10:00:54 +0100

    Create MainLayout.jsx

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/common/Navbar';
+import Footer from '../components/common/Footer';
+
+export default function MainLayout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-8 max-w-7xl">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
