06ef840b027ac97307061763134c015958ce4c88
orangescrypto-jpg <orangescrypto@gmail.com>
2026-05-07 04:41:43 +0100

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
+import LiveTicker from '../components/common/LiveTicker';
 
 export default function MainLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <LiveTicker />
       <main className="flex-grow container mx-auto px-4 py-8 max-w-7xl">
         <Outlet />
       </main>
