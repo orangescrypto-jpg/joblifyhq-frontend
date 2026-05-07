f10a34715a3bd38b8a5c7dbc5ef9ae0b2e52ad1c
orangescrypto-jpg <orangescrypto@gmail.com>
2026-05-07 10:09:52 +0100

    Create payments.js

---
+++ src/config/payments.js
@@ -0,0 +1,5 @@
+// Public keys are safe to commit - they're in your frontend anyway
+export const FLUTTERWAVE_PUBLIC_KEY = 'FLWPUBK_TEST-b9f4ac441c5dd35e07207647f19afe36-X';
+
+// When you go live, just change this one line:
+ // export const FLUTTERWAVE_PUBLIC_KEY = 'FLWPUBK-LIVE-xxxxxxxx';
