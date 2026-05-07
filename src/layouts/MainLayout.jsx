9cba9d147278abb2e3ef37015e08b0742934e1b8
orangescrypto-jpg <orangescrypto@gmail.com>
2026-05-07 04:53:49 +0100

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
 import LiveTicker from '../components/common/LiveTicker';
 
 export default function MainLayout() {
+  const location = useLocation();
+  // Only show ticker on the home page
+  const showTicker = location.pathname === '/';
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <LiveTicker />
+      {showTicker && <LiveTicker />}
       <main className="flex-grow container mx-auto px-4 py-8 max-w-7xl">
         <Outlet />
       </main>
